42ec02928a05acc1796215338a953e4f4953b83a
Krzysztof Moskalik <krzysztof.moskalik@fsi.pl>
2013-08-28 11:00:03 +0200

    checking if table exists

---
+++ objects/installer.class.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: krzysztof_moskalik
+ * Date: 28.08.13
+ * Time: 09:49
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace engine\objects;
+
+
+class installer {
+
+}
